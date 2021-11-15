c7bf9cb056d3d6f6250d530c408db7966e2f1516
Brian Rutledge <brian@bhrutledge.com>
2021-11-15 06:13:42 -0500

    Add coverage

--- test_example.py
+++ test_example.py
@@ -1,10 +1,14 @@
+import pytest
+
 import example
 
 def test_success():
     assert example.add_one(5) == 6
 
+@pytest.mark.xfail
 def test_failure():
     assert example.add_one(6) == 6
 
+@pytest.mark.xfail
 def test_exception():
     assert example.add_one(None) == 1
